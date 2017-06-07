6811c962c95bbeb4fe942f0d760aa26b2e20d5fe
Anupam Jain <ajnsit@gmail.com>
2017-06-07 16:22:01 +0530

    WIP code for typed-wire clientlib

--- src/wallet-purescript/Main.hs
+++ src/wallet-purescript/Main.hs
@@ -1,85 +1,31 @@
+{-# LANGUAGE StandaloneDeriving #-}
 module Main
        ( main
        ) where
 
-import           Language.PureScript.Bridge                (BridgePart, buildBridge,
-                                                            defaultBridge, mkSumType,
-                                                            typeName, writePSTypes, (<|>),
-                                                            (^==))
-import           Language.PureScript.Bridge.PSTypes        (psInt)
-import           Language.PureScript.Bridge.TypeParameters (A)
-import           Universum
+import Prelude
 
-import qualified Pos.Types                                 as PT
-import qualified Pos.Util.BackupPhrase                     as BP
-import qualified Pos.Wallet.Web                            as CT
+import TW.Ast
+import TW.Check
+import qualified TW.CodeGen.Haskell as HS
+import qualified TW.CodeGen.Flow as Flow
+import TW.Loader
+import TW.Parser
+import TW.Types
 
-import           PSTypes                                   (psInt53, psPosixTime)
-
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 main :: IO ()
-main =
-    writePSTypes
-      "daedalus/src/Generated"
-      (buildBridge customBridge)
-      [ mkSumType (Proxy @CT.WalletError)
-      , mkSumType (Proxy @CT.CAccountMeta)
-      , mkSumType (Proxy @CT.CAccountInit)
-      , mkSumType (Proxy @CT.CAccount)
-      , mkSumType (Proxy @CT.CWallet)
-      , mkSumType (Proxy @CT.CWalletInit)
-      , mkSumType (Proxy @CT.CProfile)
-      , mkSumType (Proxy @CT.CTxMeta)
-      , mkSumType (Proxy @CT.CTExMeta)
-      , mkSumType (Proxy @CT.CAccountId)
-      , mkSumType (Proxy @CT.CWalletMeta)
-      , mkSumType (Proxy @CT.CAddress)
-      , mkSumType (Proxy @CT.CWAddressMeta)
-      , mkSumType (Proxy @CT.Wal)
-      , mkSumType (Proxy @CT.Addr)
-      , mkSumType (Proxy @(CT.CId A))
-      , mkSumType (Proxy @CT.CHash)
-      , mkSumType (Proxy @CT.CTxId)
-      , mkSumType (Proxy @CT.CTx)
-      , mkSumType (Proxy @CT.NotifyEvent)
-      , mkSumType (Proxy @CT.SyncProgress)
-      , mkSumType (Proxy @CT.CUpdateInfo)
-      , mkSumType (Proxy @CT.CWalletRedeem)
-      , mkSumType (Proxy @CT.CPaperVendWalletRedeem)
-      , mkSumType (Proxy @CT.CInitialized)
-      , mkSumType (Proxy @CT.CPassPhrase)
-      , mkSumType (Proxy @CT.CWalletAssurance)
-      , mkSumType (Proxy @CT.CCoin)
-      , mkSumType (Proxy @PT.Coin)
-      , mkSumType (Proxy @PT.ChainDifficulty)
-      , mkSumType (Proxy @PT.BlockVersion)
-      , mkSumType (Proxy @PT.SoftwareVersion)
-      , mkSumType (Proxy @PT.ApplicationName)
-      , mkSumType (Proxy @BP.BackupPhrase)
-      ]
-  where
-      customBridge =
-          defaultBridge <|> posixTimeBridge <|> wordBridge <|>
-          word8Bridge <|> word16Bridge <|> word32Bridge <|>
-          word64Bridge
-
-posixTimeBridge :: BridgePart
-posixTimeBridge = typeName ^== "NominalDiffTime" >> pure psPosixTime
-
-wordBridge :: BridgePart
-wordBridge = typeName ^== "Word" >> pure psInt53
-
-word8Bridge :: BridgePart
-word8Bridge = typeName ^== "Word8" >> pure psInt
-
-word16Bridge :: BridgePart
-word16Bridge = typeName ^== "Word16" >> pure psInt
-
-word32Bridge :: BridgePart
-word32Bridge = typeName ^== "Word32" >> pure psInt53
-
--- FIXME: this is not actually correct, but we don't
--- use >= Int53 except for Coin, and Coin is representat
--- as String in `0.4` branch
-word64Bridge :: BridgePart
-word64Bridge = typeName ^== "Word64" >> pure psInt53
+main = do
+  T.putStrLn "Loading type modules"
+  allModules <- loadModules ["typedefs"] [ModuleName ["Pos", "Wallet", "Web", "ClientTypes"], ModuleName ["Pos", "Wallet", "Web", "Error"]]
+  T.putStrLn "All modules loaded"
+  case allModules of
+    Left err -> fail err
+    Right ok ->
+      case checkModules ok of
+        Left err -> fail err
+        Right readyModules -> do
+          mapM_ (T.putStrLn . Flow.makeModule) readyModules
+          mapM_ (T.putStrLn . HS.makeModule) readyModules
