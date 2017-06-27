943562b93ed4e0a7639b91f75bff78798b6c075c
Niklas Hambüchen <mail@nh2.me>
2017-06-27 17:41:56 +0200

    WIP: More stack travis cache debugging

--- scripts/ci/travis.sh
+++ scripts/ci/travis.sh
@@ -22,6 +22,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   export EXTRA_STACK="--flag cardano-sl:for-installer $EXTRA_STACK"
 fi
 
+ls -lah /home/travis/.stack/setup-exe-cache/x86_64-linux-nix/Cabal-simple_mPHDZzAJ_1.24.2.0_ghc-8.0.2
 stack --nix --no-terminal install happy \
   $EXTRA_STACK --fast --ghc-options="-j +RTS -A128m -n2m -RTS" --jobs=4
 
